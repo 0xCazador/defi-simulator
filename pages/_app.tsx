509d5f89fff2238bd21d92d711c32886e2fe9515
OxCazador <Oxcazador@bretthunter.com>
2023-04-26 20:48:31 -0700

    Don't set cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,14 @@
-import { useState } from 'react';
-import NextApp, { AppProps, AppContext } from 'next/app';
-import { getCookie, setCookie } from 'cookies-next';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
 import {
   MantineProvider,
   ColorScheme,
-  ColorSchemeProvider,
   MantineThemeOverride,
 } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
-    setColorScheme(nextColorScheme);
-    setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
-  };
 
   return (
     <>
@@ -33,12 +23,11 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
-          <Component {...pageProps} />
-          <Notifications />
-        </MantineProvider>
-      </ColorSchemeProvider>
+      <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+        <Notifications />
+      </MantineProvider>
+
     </>
   );
 }
